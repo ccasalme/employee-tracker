fab72b5c23f0f9a6cca6573fd4e052d25c1a73e7
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-01-06 14:55:23 -0500

    edited the indexjs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const queries = require('./queries');
+const queries = require('./queries'); // assuming your query functions are in a separate file
 
 const startApp = async () => {
     const answers = await inquirer.prompt({
@@ -14,6 +14,7 @@ const startApp = async () => {
             'Add a role',
             'Add an employee',
             'Update an employee role',
+            'Quit'
         ],
     });
 
@@ -21,8 +22,31 @@ const startApp = async () => {
         case 'View all departments':
             await queries.viewDepartments();
             break;
-        // Add cases for other actions
+        case 'View all roles':
+            await queries.viewRoles();
+            break;
+        case 'View all employees':
+            await queries.viewEmployees();
+            break;
+        case 'Add a department':
+            await queries.addDepartment();
+            break;
+        case 'Add a role':
+            await queries.addRole();
+            break;
+        case 'Add an employee':
+            await queries.addEmployee();
+            break;
+        case 'Update an employee role':
+            await queries.updateEmployeeRole();
+            break;
+        case 'Quit':
+            console.log('Goodbye!');
+            process.exit();
     }
+
+    // After completing the selected action, prompt the user again.
+    startApp();
 };
 
 startApp();
